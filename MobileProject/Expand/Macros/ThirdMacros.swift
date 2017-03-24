ca649537d89b2c08d621b02304d13386ca513205
xuwen <superman1969@sina.com>
2017-03-24 15:30:20 +0800

    修改ThirdMacros

--- MobileProject/Expand/Macros/ThirdMacros.swift
+++ MobileProject/Expand/Macros/ThirdMacros.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-//微博 accessToken
+//微博 accessToken 开放接口
 let ACCESSTOKEN = "2.00NofgBD0L1k4pc584f79cc48SKGdD"
+
+//高德地图地图SDK的Key
+let  kGaodeMapKey = "7fbc139761e51f0c5c9f8e79027d4de5"
+
+//友盟统计SDK的key
+let kUmengKey = "58d4c8f5bbea830b7900123c"
+
+//友盟分享
+//--微信
+let kSocial_WX_ID = "wxdc1e388c3822c80b"
+let kSocial_WX_Secret = "a393c1527aaccb95f3a4c88d6d1455f6"
+let kSocial_WX_Url = "http://www.umeng.com/social"
+//--QQ
+let kSocial_QQ_ID = "100424468"
+let kSocial_QQ_Secret = "c7394704798a158208a74ab60104f0ba"
+let kSocial_QQ_Url = "http://www.umeng.com/social"
+//--新浪微博
+let kSocial_Sina_Account = "3921700954"
+let kSocial_Sina_RedirectURL = "http://sns.whalecloud.com/sina2/callback"
+
+
+
+
+
