304415724b299f05bf1329cc4719c56986335ad9
xuwen <superman1969@sina.com>
2017-06-01 14:53:04 +0800

    修改桥接文件错误

--- MobileProject/Expand/Macros/Macros.swift
+++ MobileProject/Expand/Macros/Macros.swift
@@ -9,6 +9,23 @@
 import Foundation
 import UIKit
 
+//获取UDID
+var _kUdid:String?
+var kUdid:String?{
+    set {
+        _kUdid = newValue
+    }
+    
+    get {
+        _kUdid = YYQKeyChain.getUDIDWithUniqueKey(appIdentifier)
+        return _kUdid
+    }
+}
+
+//Int转String
+func formatIntToString(val:Int)->String
+{return String(format: "%d", val)}
+
 let APP_WINDOW = UIApplication.shared.keyWindow
 
 let SCREEN_WIDTH = UIScreen.main.bounds.size.width
@@ -33,6 +50,35 @@ let APP_Orange_COLOR = 0xff9600
 let APP_Black_COLOR = 0x1f302f
 let APP_BACKGROUND_COLOR = 0xf5f5f5
 
+//图片
+func kImage(imageName:String)-> UIImage {
+    return UIImage.init(named: imageName)!
+}
+
+//日志输出：
+func printLog<T>(message: T,
+              file: String = #file,
+              method: String = #function,
+              line: Int = #line) {
+    #if DEBUG
+        print("\((file as NSString).lastPathComponent)[\(line)], \(method): \(message)")
+    #endif
+}
+
+//MARK: --工程信息
+let infroDictionary = Bundle.main.infoDictionary
+//获取项目名称
+let executableFile:String = infroDictionary?[kCFBundleExecutableKey! as String] as! String
+//获取项目版本号
+let version:String = infroDictionary?[kCFBundleVersionKey! as String] as! String
+//app名称
+let appName:String = infroDictionary?["CFBundleDisplayName"] as! String
+//app版本
+let appVersion:String = infroDictionary?["CFBundleShortVersionString"] as! String
+// app build版本
+let appBuild:String = infroDictionary?["CFBundleVersion"] as! String
+// app bundleID
+let appIdentifier:String = infroDictionary?["CFBundleIdentifier"] as! String
 
 
 
--- MobileProject/Supporting Files/MobileProject-Bridging-Header.h
+++ MobileProject/Supporting Files/MobileProject-Bridging-Header.h
@@ -2,7 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-
 //友盟
 //#import <UMSocial.h>
 #import <UMSocialWechatHandler.h>
@@ -10,3 +9,30 @@
 #import <UMSocialSinaHandler.h>
 //#import <UMSocialSinaSSOHandler.h>
 #import "UMMobClick/MobClick.h"
+
+//数据库
+#import "FMDB.h"
+#import "FMDBMigrationManager.h"
+#import "Migration.h"
+
+#import "FCFileManager.h"
+
+//下拉刷新
+//#import "MJRefresh.h"
+//cell自动适应高度
+//#import "UITableView+HYBCacheHeight.h"
+//#import "UITableViewCell+HYBMasonryAutoCellHeight.h"
+//带带头部和尾部的瀑布流
+//#import "ARCollectionViewMasonryLayout.h"
+
+//UDID
+#import "YYQKeyChain.h"
+
+//MD5
+#import <CommonCrypto/CommonDigest.h>
+
+//自动化布局
+#import "Masonry.h"
+
+//手势返回用
+#import "UINavigationController+FDFullscreenPopGesture.h"
